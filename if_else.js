470f4664daba47cd6029de6869e6c5d06f65ea32
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-10 23:22:35 +0530

    [Akash]ADDED: Taking inputs from cmd line and printing true if day of month is between March 20 and June 20, false otherwise

--- if_else.js
+++ if_else.js
@@ -31,3 +31,29 @@
     console.log("The minimum number is: " + minNum);
     console.log("The maximum number is: " + maxNum);
 }
+
+// Printing true if day of month is between March 20 and June 20, false otherwise
+{
+    const prompt = require('prompt-sync')();
+    const day = prompt('Enter day: ');
+    const month = prompt('Enter month: ');
+    if(day >= 20 && day <= 31 && month == "March") {
+        console.log("True");
+        return;
+    }
+       
+    if(day >= 1 && day <= 30 && month == "April") {
+        console.log("True");
+        return;
+    }
+    if(day >= 1 && day <= 31 && month == "May"){
+        console.log("True");
+        return;
+    }
+    if(day >= 1 && day <= 20 && month == "June") {
+        console.log("True");
+        return;
+    }
+    else 
+        console.log("False");
+}
