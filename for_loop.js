b1014ef1be11fd565a9ec11dadba6e9021ca891a
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-11 03:13:54 +0530

    [Akash]ADDED: Printing all the existing prime numbers between a range of given numbers

--- for_loop.js
+++ for_loop.js
@@ -43,3 +43,22 @@
         }
     }              
 }
+
+// Printing prime numbers between a range of given numbers
+{
+    const prompt = require('prompt-sync')();
+    let lowerNumber = Number(prompt('Enter the lower number of range: '));
+    let higherNumber = Number(prompt('Enter the higher number of range: '));
+    console.log("The prime numbers between " + lowerNumber + " and " + higherNumber + " are: ");
+    for(let pos = lowerNumber; pos <= higherNumber; pos++) {
+        let flag = 0;
+        for (let position = 2; position < pos; position++) {
+            if (pos % position == 0) {
+                flag = 1;
+                break;                
+            }
+        }
+        if (pos > 1 && flag == 0)
+                console.log(pos);
+    } 
+}
