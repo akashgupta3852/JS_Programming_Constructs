a7372526007829f7e199991bef8f0c0e15099aff
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-10 21:34:02 +0530

    [Akash]ADDED: Printing the sum of two dice numbers

--- sequence.js
+++ sequence.js
@@ -1,7 +1,15 @@
-//Getting single digit number
+//Printing the single digit number
 let digit = Math.floor(Math.random() * 10);
-console.log("The single digit number is: "+digit);
+console.log("The single digit number is: " + digit);
 
-//Getting dice number between 1 to 6
+//Printing the dice number between 1 to 6
 let diceNumber = Math.floor(Math.random() * 10) % 6 + 1;
 console.log("The dice number after rolling is: " + diceNumber);
+
+//Printing the sum of two dice numbers
+let diceNumber1 = Math.floor(Math.random() * 10) % 6 + 1;
+let diceNumber2 =Math.floor(Math.random() * 10) % 6 + 1;
+let sum = diceNumber1 + diceNumber2;
+console.log("The 1st dice number after rolling is: " + diceNumber1);
+console.log("The 2nd dice number after rolling is: " + diceNumber2);
+console.log("The sum of both dice numbers is: " + sum);
