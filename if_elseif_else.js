eaca6691d22836b06cdf03ea0a1c194369772e9b
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-11 00:10:06 +0530

    [Akash]ADDED: Printing the single digit number in the word

---
+++ if_elseif_else.js
@@ -0,0 +1,24 @@
+// Getting a single digit number and printing them in the word
+{
+    let num = Math.floor(Math.random() * 10);
+    if(num == 0)
+        console.log("The number : " + num + " in word is Zero"); 
+    else if(num == 1)
+        console.log("The number : " + num + " in word is One");
+    else if(num == 2)
+        console.log("The number : " + num + " in word is Two");
+    else if(num == 3)
+        console.log("The number : " + num + " in word is Three");
+    else if(num == 4)
+        console.log("The number : " + num + " in word is Four");
+    else if(num == 5)
+        console.log("The number : " + num + " in word is Five");
+    else if(num == 6)
+        console.log("The number : " + num + " in word is Six");
+    else if(num == 7)
+        console.log("The number : " + num + " in word is Seven");
+    else if(num == 8)
+        console.log("The number : " + num + " in word is Eight");
+    else
+        console.log("The number : " + num + " in word is Nine");
+}
