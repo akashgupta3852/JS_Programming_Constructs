eef43dc25d37670fac94599afae5780cc5503a18
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-11 03:21:45 +0530

    [Akash]ADDED: Taking number n as input from cmd line and printing its factorial value

--- for_loop.js
+++ for_loop.js
@@ -62,3 +62,13 @@
                 console.log(pos);
     } 
 }
+
+// Calculating factorial of a number
+{
+    const prompt = require('prompt-sync')();
+    let num = Number(prompt('Enter the number for calculating its factorial: '));
+    let factorialValue = 1;
+    for(let pos = 1; pos <= num; pos++)
+         factorialValue *= pos;
+    console.log("The factorial of the given number: " + num + " is " + factorialValue);
+}
