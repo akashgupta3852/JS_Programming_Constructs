cc15e5562b626987ceb062007f58197baca82b92
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-10 21:42:16 +0530

    [Akash]ADDED: Printing the sum and average of 5 numbers

--- sequence.js
+++ sequence.js
@@ -13,3 +13,13 @@ let sum = diceNumber1 + diceNumber2;
 console.log("The 1st dice number after rolling is: " + diceNumber1);
 console.log("The 2nd dice number after rolling is: " + diceNumber2);
 console.log("The sum of both dice numbers is: " + sum);
+
+//Printing the sum and average of 5 numbers
+let number1 = Math.floor(Math.random() * 100);
+let number2 = Math.floor(Math.random() * 100);
+let number3 = Math.floor(Math.random() * 100);
+let number4 = Math.floor(Math.random() * 100);
+let number5 = Math.floor(Math.random() * 100);
+let result = number1 + number2 + number3 + number4 + number5;
+let average = result / 5;
+console.log("The sum of 5 numbers is: "+ result+ " and their average is: "+average); 
