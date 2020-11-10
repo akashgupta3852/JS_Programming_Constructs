ba17a514e782b55e4bf4cdeb00120ced94185863
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-11 00:40:48 +0530

    [Akash]ADDED: Reading a number from cmd line and printing its unit, tens, hundreds,… digit

--- if_elseif_else.js
+++ if_elseif_else.js
@@ -1,43 +1,63 @@
-// Getting a single digit number and printing them in the word
+// Reading a single digit number from cmd line and printing them in the word
 {
-    let num = Math.floor(Math.random() * 10);
+    const prompt = require('prompt-sync')();
+    const num = prompt('Enter a single digit number: ');
     if(num == 0)
-        console.log("The number : " + num + " in word is Zero"); 
+        console.log("The number is Zero"); 
     else if(num == 1)
-        console.log("The number : " + num + " in word is One");
+        console.log("The number is One");
     else if(num == 2)
-        console.log("The number : " + num + " in word is Two");
+        console.log("The number is Two");
     else if(num == 3)
-        console.log("The number : " + num + " in word is Three");
+        console.log("The number is Three");
     else if(num == 4)
-        console.log("The number : " + num + " in word is Four");
+        console.log("The number is Four");
     else if(num == 5)
-        console.log("The number : " + num + " in word is Five");
+        console.log("The number is Five");
     else if(num == 6)
-        console.log("The number : " + num + " in word is Six");
+        console.log("The number is Six");
     else if(num == 7)
-        console.log("The number : " + num + " in word is Seven");
+        console.log("The number is Seven");
     else if(num == 8)
-        console.log("The number : " + num + " in word is Eight");
+        console.log("The number is Eight");
+    else if(num == 9)
+        console.log("The number is Nine");
     else
-        console.log("The number : " + num + " in word is Nine");
+        console.log("It is invalid number");
 }
 
-// Getting a day number and printing its day name accordingly
+// Reading a day number from cmd line and printing its day name accordingly
 {
-    let dayNum = Math.floor(Math.random() *10) % 7 + 1;
+    const prompt = require('prompt-sync')();
+    const dayNum = prompt('Enter day number between 1 to 7: ');
     if(dayNum == 1)
-        console.log("The day: " + dayNum + " is Monday");
+        console.log("The day is Monday");
     else if(dayNum == 2)
-        console.log("The day: " + dayNum + " is Tuesday");
+        console.log("The day is Tuesday");
     else if(dayNum == 3)
-        console.log("The day: " + dayNum + " is Wednesday");
+        console.log("The day is Wednesday");
     else if(dayNum == 4)
-        console.log("The day: " + dayNum + " is Thursday");
+        console.log("The day is Thursday");
     else if(dayNum == 5)
-        console.log("The day: " + dayNum + " is Friday");
+        console.log("The day is Friday");
     else if(dayNum == 6)
-        console.log("The day: " + dayNum + " is Saturday");
+        console.log("The day is Saturday");
+    else if(dayNum == 7)
+        console.log("The day is Sunday");
     else
-        console.log("The day: " + dayNum + " is Sunday"); 
+        console.log("It is invalid number"); 
+}
+
+// Reading a number from cmd line and displaying its unit, tens, hundreds,…
+{
+    const prompt = require('prompt-sync')();
+    const numInput = prompt('Enter a number: ');
+    var num = numInput;
+    console.log("Its unit, tens, hundreds,... digit is");
+    while(num > 0) {
+        let rem = num % 10;
+        console.log(rem);
+        num = num / 10;
+        num = Math.floor(num);
+    }
 }
