cbe3e335a3bdfe576305058d19edc3797855eaf5
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-11 00:18:55 +0530

    [Akash]ADDED: Printing the name of day according to its day number

--- if_elseif_else.js
+++ if_elseif_else.js
@@ -22,3 +22,22 @@
     else
         console.log("The number : " + num + " in word is Nine");
 }
+
+// Getting a day number and printing its day name accordingly
+{
+    let dayNum = Math.floor(Math.random() *10) % 7 + 1;
+    if(dayNum == 1)
+        console.log("The day: " + dayNum + " is Monday");
+    else if(dayNum == 2)
+        console.log("The day: " + dayNum + " is Tuesday");
+    else if(dayNum == 3)
+        console.log("The day: " + dayNum + " is Wednesday");
+    else if(dayNum == 4)
+        console.log("The day: " + dayNum + " is Thursday");
+    else if(dayNum == 5)
+        console.log("The day: " + dayNum + " is Friday");
+    else if(dayNum == 6)
+        console.log("The day: " + dayNum + " is Saturday");
+    else
+        console.log("The day: " + dayNum + " is Sunday"); 
+}
