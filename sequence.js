6ea698fd681aa6aceba38327414a6689bfce72c0
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-10 21:26:25 +0530

    [Akash]ADDED: Random function to get dice number between 1 to 6

--- sequence.js
+++ sequence.js
@@ -1,2 +1,7 @@
+//Getting single digit number
 let digit = Math.floor(Math.random() * 10);
 console.log("The single digit number is: "+digit);
+
+//Getting dice number between 1 to 6
+let diceNumber = Math.floor(Math.random() * 10) % 6 + 1;
+console.log("The dice number after rolling is: " + diceNumber);
