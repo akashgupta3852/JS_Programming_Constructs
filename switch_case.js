3280aa51598729d2602431dcb5df3aeb1acb01de
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-11 01:25:00 +0530

    [Akash]ADDED: Reading a day number from cmd line and using switch case statements, printing its day name accordingly

--- switch_case.js
+++ switch_case.js
@@ -37,3 +37,34 @@
             console.log("Invalid Input");
     }
 } 
+
+// Reading a day number from cmd line and printing its day name accordingly
+{
+    const prompt = require('prompt-sync')();
+    const dayNum = prompt('Enter day number between 1 to 7: ');
+    switch(dayNum) {
+        case "1" :
+            console.log("Monday");
+            break;
+        case "2" :
+            console.log("Tuesday");
+            break;
+        case "3" :
+            console.log("Wednesday");
+            break;
+        case "4" :
+            console.log("Thursday");
+            break;
+        case "5" :
+            console.log("Friday");
+            break;
+        case "6" :
+            console.log("Saturday");
+            break;
+        case "7" :
+            console.log("Sunday");
+            break;
+        default:
+            console.log("Invalid day");
+    } 
+}
