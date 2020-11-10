fdec3788d136c9b142f4a23ef5a2f5bda28e589a
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-11 02:33:25 +0530

    [Akash]ADDED: Taking a number n as input from cmd line and printing nth Harmonic number

--- for_loop.js
+++ for_loop.js
@@ -1,12 +1,24 @@
 // Printing a table of the powers of 2 that are less than or equal to 2^n
 {
     const prompt = require('prompt-sync')();
-    const numInput = prompt('Enter a number: ');
-    var num = Number(numInput);
+    let numInput = prompt('Enter a number: ');
+    let num = Number(numInput);
     console.log("The table of the powers of 2 that are less than or equal to 2^" + num);
     let power = 1;
     for(let position = 0 ; position < num ; position++) {
         power = power * 2;
         console.log(power);
     }
+}
+
+// Printing nth Harmonic number
+{
+    const prompt = require('prompt-sync')();
+    let numInput = prompt('Enter a number : ');
+    let num = Number(numInput);
+    let harmonicNum = 0; 
+    for (let position = 1; position <= num; position++) { 
+        harmonicNum += 1 / position; 
+    } 
+    console.log("The Harmonic number for given value is: "+ harmonicNum);
 }
