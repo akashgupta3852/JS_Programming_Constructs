25587d607e0a1a38346800fe4f97dfc5917c1e04
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-11 22:55:57 +0530

    [Akash]ADDED: Simulating a coin flip and printing heads or tails accordingly 11 times

--- while_loop.js
+++ while_loop.js
@@ -30,3 +30,26 @@
     }
     console.log("The assumed number is: " + num);
 }
+
+// Simulating a coin flip and printing heads or tails accordingly 11 times
+{
+    const HEAD = 0;
+    const TAIL = 1;
+    let headCount = 0;
+    let tailCount = 0;
+    let tossCount = 0;
+    while(tossCount < 11) {
+        let toss = Math.floor(Math.random() * 10) % 2;
+        if(toss == HEAD) {  
+            console.log("Head")
+            headCount++;
+        }
+        else if (toss == TAIL) {
+            console.log("Tail")
+            tailCount++;
+        }
+        tossCount++;
+    }
+    console.log("Total Head counts: "+ headCount);
+    console.log("Total Tail counts: "+ tailCount);
+}
