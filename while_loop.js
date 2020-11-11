69500540840c925ccf0c532112c26a7becde58d1
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-11 18:23:50 +0530

    [Akash]ADDED: Finding the magic number that was thought by the user

--- while_loop.js
+++ while_loop.js
@@ -10,3 +10,23 @@
         pos++;
     }
 }
+
+// Finding the magic number
+{
+    const prompt = require('prompt-sync')();
+    let userNum = Number(prompt('Think of a number n between 1 to 100: '));
+    let num = 50;
+    let lowerNum = 0;
+    let higherNum = 100;
+    while(num != userNum) {
+        if(num > userNum) {
+            higherNum = num;
+            num = Math.floor((lowerNum + higherNum) / 2);
+        }
+        if(num < userNum) {
+            lowerNum = num;
+            num = Math.floor((lowerNum + higherNum) / 2);
+        }
+    }
+    console.log("The assumed number is: " + num);
+}
