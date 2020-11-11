77f1277b2e72a82404a1078e082023b307c69ea7
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-11 17:20:13 +0530

    [Akash]REFACTORED: The variable name, user input to Number datatype and the logic for finding maximum and minimum number

--- if_elseif_else.js
+++ if_elseif_else.js
@@ -1,7 +1,7 @@
 // Reading a single digit number from cmd line and printing them in the word
 {
     const prompt = require('prompt-sync')();
-    const num = prompt('Enter a single digit number: ');
+    let num = prompt('Enter a single digit number: ');
     if(num == 0)
         console.log("The number is Zero"); 
     else if(num == 1)
@@ -29,7 +29,7 @@
 // Reading a day number from cmd line and printing its day name accordingly
 {
     const prompt = require('prompt-sync')();
-    const dayNum = prompt('Enter day number between 1 to 7: ');
+    let dayNum = prompt('Enter day number between 1 to 7: ');
     if(dayNum == 1)
         console.log("The day is Monday");
     else if(dayNum == 2)
@@ -51,8 +51,7 @@
 // Reading a number from cmd line and displaying its unit, tens, hundreds,…
 {
     const prompt = require('prompt-sync')();
-    const numInput = prompt('Enter a number: ');
-    var num = numInput;
+    let num = Number(prompt('Enter a number: '));
     console.log("Its unit, tens, hundreds,... digit is");
     while(num > 0) {
         let rem = num % 10;
@@ -65,12 +64,9 @@
 // Taking 3 numbers from cmd line and printing the max and min value after doing arithmetic operations
 {
     const prompt = require('prompt-sync')();
-    const numInput1 = prompt('Enter 1st number: ');
-    const numInput2 = prompt('Enter 2nd number: ');
-    const numInput3 = prompt('Enter 3rd number: ');
-    var a = numInput1;
-    var b = numInput2;
-    var c = numInput3;
+    let a = Number(prompt('Enter 1st number: '));
+    let b = Number(prompt('Enter 2nd number: '));
+    let c = Number(prompt('Enter 3rd number: '));
     console.log("The three numbers are: " + a + ", " + b + " and " + c);
     let result1 = a + b * c;
     console.log(result1);
@@ -80,22 +76,20 @@
     console.log(result3);
     let result4 = a * b + c;
     console.log(result4);
-
     let min = result1;
     if(result2 < min)
         min = result2;
-    else if(result3 < min)
+    if(result3 < min)
         min = result3;
-    else if(result4 < min)
+    if(result4 < min)
         min = result4;
     console.log("The minimum value is: " + min);
-
     let max = result1;
     if(result2 > max)
         max = result2;
-    else if(result3 > max)
+    if(result3 > max)
         max = result3;
-    else if(result4 > max)
+    if(result4 > max)
         max = result4;
     console.log("The maximum value is: " + max);
 }
