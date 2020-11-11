02398da7e4d673ddeea7dbeb50adbd9fcdf88984
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-11 23:22:58 +0530

    [Akash]ADDED: Getting track of number of times won and number of bets made by gambler

--- while_loop.js
+++ while_loop.js
@@ -38,6 +38,7 @@
     let headCount = 0;
     let tailCount = 0;
     let tossCount = 0;
+    console.log("Welcome to Flipping Coin Program");
     while(tossCount < 11) {
         let toss = Math.floor(Math.random() * 10) % 2;
         if(toss == HEAD) {  
@@ -52,4 +53,25 @@
     }
     console.log("Total Head counts: "+ headCount);
     console.log("Total Tail counts: "+ tailCount);
+}
+
+// Getting track of number of times won and number of bets made by gambler
+{
+    const BET_AMOUNT = 1;
+    const WIN = 1;
+    let winCount = 0, betCount = 0, balance = 100;
+    console.log("Welcome to Gambling Program");
+    while(balance < 200 && balance > 0) {
+        let gamble = Math.floor(Math.random() * 10) % 2;
+        if(gamble == WIN) {
+            balance = balance + BET_AMOUNT;
+            winCount++;
+        }
+        else
+            balance = balance - BET_AMOUNT;
+        betCount++;
+    }
+    console.log("Total number of bets made: "+ betCount);
+    console.log("Total wins by gambler = " + winCount);
+    console.log("Final money of gambler= " + balance);
 }
