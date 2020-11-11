e77e8f18234aa2330fc0db44ee45d9b204598c5e
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-12 03:31:00 +0530

    [Akash]ADDED: Finding all the individuals having birthdays in the same month

--- map.js
+++ map.js
@@ -35,20 +35,20 @@
     console.log("The dice number: "+ diceNumForMaxTimes + " is rolled for maximum times i.e. " + 10);
 }
 
-// // Finding all the individuals having birthdays in the same month
-// {
-//     console.log("Welcome to Born Month Program");
-//     let monthMap = new Map();
-//     for (let monthNum = 1; monthNum <= 12; monthNum++) {
-//         monthMap.set(monthNum, 0);
-//     }
+// Finding all the individuals having birthdays in the same month
+{
+    console.log("Welcome to Born Month Program");
+    let monthMap = new Map();
+    for (let monthNum = 1; monthNum <= 12; monthNum++) {
+        monthMap.set(monthNum, 0);
+    }
 
-//     for(let numOfPeople = 1; numOfPeople <= 50; numOfPeople++) {
-//         let monthNum = (Math.floor(Math.random() * 100) % 12) + 1;
-//         let count = monthMap.get(monthNum);
-//         monthMap.set(monthNum, ++count);
-//     }
-//     for (let monthNum = 1; monthNum <= 12; monthNum++) {
-//         console.log("No. of people born in month num: "+ monthNum + " is " +monthMap.get(monthNum));
-//     }
-// }
+    for(let numOfPeople = 1; numOfPeople <= 50; numOfPeople++) {
+        let monthNum = (Math.floor(Math.random() * 100) % 12) + 1;
+        let count = monthMap.get(monthNum);
+        monthMap.set(monthNum, ++count);
+    }
+    for (let monthNum = 1; monthNum <= 12; monthNum++) {
+        console.log("No. of people born in month num: "+ monthNum + " is " +monthMap.get(monthNum));
+    }
+}
