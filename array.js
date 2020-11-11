1fb559a3049e8f3159b09cdc7fe4d95338b6fa28
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-12 01:29:36 +0530

    [Akash]ADDED: Finding factors of a number using prime factorisation method and storing them in a array

--- array.js
+++ array.js
@@ -44,3 +44,19 @@
     console.log("The 2nd smallest number is " + numArray[1]);
     console.log("The 2nd largest number is " + numArray[numArray.length - 2]);
 }
+
+// Finding factors of a number using prime factorisation method and storing them in a array.
+{
+    console.log("Welcome to Prime Factorisation Program");
+    const prompt = require('prompt-sync')();
+    let num = Number(prompt('Enter the number for finding its prime factor: '));
+    console.log("The prime factors of " + num + " are: ");
+    let factorArray = new Array();
+    for(let pos = 2; pos < num; pos++) {
+        for( ; num % pos == 0 ; num /= pos)
+            factorArray.push(pos);
+     }
+     if(num > 2)
+        factorArray.push(num);
+    console.log(factorArray);
+}
