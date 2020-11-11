09d18da0ad3477bd335b6ced68a321ca4e2a3faa
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-11 17:10:30 +0530

    [Akash]ADDED: Taking number n as input from cmd line and printing its prime factors

--- for_loop.js
+++ for_loop.js
@@ -72,3 +72,18 @@
          factorialValue *= pos;
     console.log("The factorial of the given number: " + num + " is " + factorialValue);
 }
+
+// Finding factors of a number using prime factorisation method
+{
+    const prompt = require('prompt-sync')();
+    let num = Number(prompt('Enter the number for finding its prime factor: '));
+    console.log("The prime factors of " + num + " are: ");
+    for(let pos = 2; pos < num; pos++) {
+        for( ; num % pos == 0 ; num /= pos) {
+            console.log(pos + " ");
+        }
+     }
+     if(num > 2) {
+        console.log(num);
+     }
+}
