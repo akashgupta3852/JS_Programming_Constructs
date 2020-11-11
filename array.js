c8979efa4f497b41a827f7dc99c8fd3ea36851ea
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-12 01:51:43 +0530

    [Akash]ADDED: Showing all the triplets of integers that sums to ZERO

--- array.js
+++ array.js
@@ -60,3 +60,32 @@
         factorArray.push(num);
     console.log(factorArray);
 }
+
+// Program to show Sum of three Integer adds to ZERO.
+{
+    console.log("Welcome to Finding Triplet Program")
+    let arr = new Array(0, -1, 2, -3, 1);
+    let size = arr.length;
+    console.log("Sum of three integers adding to zero: ")
+    findTriplet(arr , size);
+
+    function findTriplet(arr, n) {
+        let found = true; 
+        for (let i=0; i<n-2; i++) 
+        { 
+            for (let j=i+1; j<n-1; j++) 
+            { 
+                for (let k=j+1; k<n; k++) 
+                { 
+                    if (arr[i]+arr[j]+arr[k] == 0) 
+                    { 
+                        console.log(arr[i] + " " + arr[j] + " " + arr[k]);
+                        found = true; 
+                    } 
+                } 
+            } 
+        } 
+        if (found == false) 
+            console.log("There does not exist any triplets"); 
+    }
+}
